2bcb8ac06a6bd25361671dbe0269c755f638136e
EmanuelCAC <emanuelc279@gmail.com>
2024-03-14 10:12:33 -0300

    Setting the project

---
+++ src/Routes.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { useRoutes } from "react-router-dom";
+import Home from "pages/Home";
+import NotFound from "pages/NotFound";
+import EduviCoursesPricing from "pages/EduviCoursesPricing";
+import EduviJoinAsTeacher from "pages/EduviJoinAsTeacher";
+import Allmentors from "pages/Allmentors";
+import Singlementordetails from "pages/Singlementordetails";
+import EduviCoursesDetails from "pages/EduviCoursesDetails";
+import EduviShop from "pages/EduviShop";
+import EduviCourses from "pages/EduviCourses";
+
+const ProjectRoutes = () => {
+  let element = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "*", element: <NotFound /> },
+    {
+      path: "eduvicoursespricing",
+      element: <EduviCoursesPricing />,
+    },
+    {
+      path: "eduvijoinasteacher",
+      element: <EduviJoinAsTeacher />,
+    },
+    {
+      path: "allmentors",
+      element: <Allmentors />,
+    },
+    {
+      path: "singlementordetails",
+      element: <Singlementordetails />,
+    },
+    {
+      path: "eduvicorsesdetails",
+      element: <EduviCoursesDetails />,
+    },
+    {
+      path: "eduvishop",
+      element: <EduviShop />,
+    },
+    {
+      path: "eduvicourses",
+      element: <EduviCourses />,
+    },
+  ]);
+
+  return element;
+};
+
+export default ProjectRoutes;
