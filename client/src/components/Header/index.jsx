028d4f58d14c15df93c5cc0f33988276a25e26db
EmanuelCAC <emanuelc279@gmail.com>
2024-03-21 13:47:05 -0300

    setting the login page

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,63 +1,136 @@
-import React from "react";
+import React, { useState } from "react";
 import { Img, Text } from "./..";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LogIn from "modals/LogIn";
+import SignUp from "modals/SignUp";
 
 export default function Header({ ...props }) {
   const navigate = useNavigate();
+  const authStatus = useSelector((state) => state.auth.status)
+  const [login, setLogin] = useState(false)
+
+  const navItems = [
+    {
+      name: "Shop",
+      slug: "/shop",
+      active: true
+    },
+    {
+      name: "For Kindergarten",
+      slug: "#",
+      img: {
+        src: "images/img_arrow_down.svg",
+        alt: "arrowdown_one",
+        className: "h-[24px] w-[24px]"
+      },
+      active: true
+    },
+    {
+      name: "For High School",
+      slug: "#",
+      img: {
+        src: "images/img_arrow_down.svg",
+        alt: "arrowdown_one",
+        className: "h-[24px] w-[24px]"
+      },
+      active: true
+    },
+    {
+      name: "For College",
+      slug: "#",
+      img: {
+        src: "images/img_arrow_down.svg",
+        alt: "arrowdown_one",
+        className: "h-[24px] w-[24px]"
+      },
+      active: true
+    },
+    {
+      name: "Courses",
+      slug: "#",
+      img: {
+        src: "images/img_arrow_down.svg",
+        alt: "arrowdown_one",
+        className: "h-[24px] w-[24px]"
+      },
+      active: true
+    },
+    {
+      name: "Mentors",
+      slug: "#",
+      active: true
+    }
+  ]
+
+  const navAuthItems = [
+    {
+      name: "Cart (0)",
+      slug: "#",
+      img: {
+        src: "images/img_shopping_bag_24.svg",
+        alt: "shoppingbagtwen",
+        className: "h-[30px] w-[30px]"
+      },
+      action: () => { navigate(item.slug) },
+      active: authStatus
+    },
+    {
+      name: "My Account",
+      slug: "#",
+      img: {
+        src: "images/img_profile_24_outline.svg",
+        alt: "profiletwentyfo",
+        className: "h-[30px] w-[30px]"
+      },
+      action: () => { navigate(item.slug) },
+      active: authStatus
+    },
+    {
+      name: "Login",
+      slug: "#",
+      img: {
+        src: "images/img_profile_24_outline.svg",
+        alt: "profiletwentyfo",
+        className: "h-[30px] w-[30px]"
+      },
+      action: () => { setLogin(true) },
+      active: !authStatus
+    },
+  ]
 
   return (
     <header {...props}>
-      <div className="flex flex-row md:flex-col justify-between w-full mx-auto md:gap-10 md:px-5 max-w-7xl">
+      <div className="flex flex-row md:flex-col justify-between w-full mx-auto md:gap-10 md:px-5 max-w-7xl" onClick={() => { if (login) setLogin(false) }}>
         <button onClick={() => navigate("/")}>
           <Img src="images/img_logo.svg" alt="logo_one" className="h-[30px]" />
         </button>
-        <div className="flex flex-row md:flex-col justify-start items-center w-[58%] md:w-full gap-6 md:gap-5">
-          <Text as="button" className="!text-gray-900 !font-medium" onClick={() => navigate("/shop")}>
-            Shop
-          </Text>
-          <div className="flex flex-row justify-start items-center w-[22%] md:w-full gap-0.5">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              For Kindergarten
-            </Text>
-            <Img src="images/img_arrow_down.svg" alt="arrowdown_one" className="h-[24px] w-[24px]" />
-          </div>
-          <div className="flex flex-row justify-start items-center w-[21%] md:w-full gap-0.5 py-[3px]">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              For High School
-            </Text>
-            <Img src="images/img_arrow_down.svg" alt="arrowdown_three" className="h-[24px] w-[24px]" />
-          </div>
-          <div className="flex flex-row justify-start items-center w-[16%] md:w-full gap-0.5 py-[3px]">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              For College
-            </Text>
-            <Img src="images/img_arrow_down.svg" alt="arrowdown_five" className="h-[24px] w-[24px]" />
-          </div>
-          <div className="flex flex-row justify-start items-center w-[13%] md:w-full gap-0.5 py-[3px]">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              Courses
-            </Text>
-            <Img src="images/img_arrow_down.svg" alt="arrowdown_seven" className="h-[24px] w-[24px]" />
-          </div>
-          <Text as="p" className="!text-gray-900 !font-medium">
-            Mentors
-          </Text>
-        </div>
-        <div className="flex flex-row justify-between w-[21%] md:w-full">
-          <div className="flex flex-row justify-start items-center gap-2.5">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              Cart (0)
-            </Text>
-            <Img src="images/img_shopping_bag_24.svg" alt="shoppingbagtwen" className="h-[30px] w-[30px]" />
-          </div>
-          <div className="flex flex-row justify-start items-center pl-[3px] gap-2.5">
-            <Text as="p" className="!text-gray-900 text-right !font-medium">
-              My Account
-            </Text>
-            <Img src="images/img_profile_24_outline.svg" alt="profiletwentyfo" className="h-[30px] w-[30px]" />
-          </div>
-        </div>
+        <ul className="flex flex-row md:flex-col justify-start items-center w-[58%] md:w-full gap-6 md:gap-5">
+          {
+            navItems.map((item) => item.active ? (
+              <li key={item.name} className="flex flex-row justify-start items-center">
+                <Text as="button" className="!text-gray-900 !font-medium" onClick={() => navigate(item.slug)}>
+                  {item.name}
+                </Text>
+                {item.img && (<Img {...item.img} />)}
+              </li>
+            ) : null)
+          }
+        </ul>
+        <ul className="flex flex-row justify-between w-[21%] md:w-full">
+          {
+            navAuthItems.map((item) => item.active ? (
+              <li key={item.name} className="flex flex-row justify-start items-center pl-[3px] gap-2.5 ml-auto" onClick={() => item.action()}>
+                <Text as="button" className="!text-gray-900 text-right !font-medium" >
+                  {item.name}
+                </Text>
+                <Img {...item.img} />
+              </li>
+            ) : null)
+          }
+        </ul>
       </div>
+      <LogIn isOpen={login} />
     </header>
   );
 }
