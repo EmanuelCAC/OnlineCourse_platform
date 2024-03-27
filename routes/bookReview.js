7fd3a47c3dead5119a52df878f053a2c4fb482c1
EmanuelCAC <emanuelc279@gmail.com>
2024-03-27 15:10:28 -0300

    setting the authentication middleware

--- routes/bookReview.js
+++ routes/bookReview.js
@@ -1,9 +1,10 @@
 import express from 'express'
+import authenticateUser from '../middleware/authentication.js'
 import { getAll, create, getOne, edit, remove } from '../controllers/bookReview.js'
 
 const router = express.Router()
 
-router.route('/').get(getAll).post(create)
-router.route('/:id').get(getOne).patch(edit).delete(remove)
+router.route('/').get(getAll).post(authenticateUser, create)
+router.route('/:id').get(getOne).patch(authenticateUser, edit).delete(authenticateUser, remove)
 
 export default router
