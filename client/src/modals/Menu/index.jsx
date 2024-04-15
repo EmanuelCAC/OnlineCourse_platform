809e0170fa425baf3934fe9e3491efbfb6b03413
EmanuelCAC <emanuelc279@gmail.com>
2024-04-15 09:43:33 -0300

    close button and design cahnges on menu

--- client/src/modals/Menu/index.jsx
+++ client/src/modals/Menu/index.jsx
@@ -21,7 +21,7 @@ export default function Menu({ isOpen, close, ...props }) {
       preventScroll={true}
       appElement={document.getElementById("root")}
       isOpen={isOpen}
-      className="w-[25%] h-[100vh] ml-auto translate-x-[100%] rounded-l-[15px]"
+      className="w-[20%] h-[100vh] ml-auto translate-x-[100%] rounded-l-[15px]"
       id="menu"
       style={{
         overlay: {
@@ -30,7 +30,12 @@ export default function Menu({ isOpen, close, ...props }) {
       }}
     >
       <div className="flex flex-row justify-center w-full h-full">
-        <div className="flex flex-row justify-center w-full px-6 py-8 bg-white-A700 rounded-l-[15px] outline-none">
+        <div className="flex flex-row justify-center w-full px-6 py-8 bg-white-A700 rounded-l-[15px] outline-none  relative">
+          <Button
+            shape="round"
+            onClick={() => close()}
+            className="!h-[37px] !w-[37px] !p-0 text-2xl bg-transparent text-gray-300 hover:bg-gray-100 hover:text-gray-600 absolute right-4 top-4"
+          >&#10799;</Button>
           <div className="flex flex-row justify-center w-full">
             <div className="flex flex-col w-full gap-4">
               <div className="flex flex-col w-full items-center">
@@ -39,14 +44,18 @@ export default function Menu({ isOpen, close, ...props }) {
               </div>
               <div className="flex flex-col w-full h-full gap-1">
                 <hr />
-                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-200 w-full p-1.5 rounded-md text-left">Your books</Text>
-                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-200 w-full p-1.5 rounded-md text-left">Your courses</Text>
-                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-200 w-full p-1.5 rounded-md text-left">Your certificates</Text>
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Your profile</Text>
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Add account</Text>
                 <hr />
-                <Text as="button" className="text-gray-600 mt-auto border border-transparent hover:bg-gray-200 w-full p-1.5 rounded-md text-left">Settings</Text>
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Your books</Text>
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Your courses</Text>
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Your certificates</Text>
+                <hr />
+                <Text as="button" className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Settings</Text>
+                <Text as="button" className="text-gray-600 mt-auto border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left">Support</Text>
                 <Text
                   as="button"
-                  className="text-gray-600 border border-transparent hover:bg-gray-200 w-full p-1.5 rounded-md text-left"
+                  className="text-gray-600 border border-transparent hover:bg-gray-100 w-full p-1.5 rounded-md text-left"
                   onClick={() => {
                     dispatch(authLogut())
                     localStorage.removeItem('token')
