d77802589c8e32b38485b9722de9490aaea40e5b
EmanuelCAC <emanuelc279@gmail.com>
2024-06-03 11:22:45 -0300

    responsive header

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Img, Text } from "./..";
+import { Button, Img, Text } from "./..";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
@@ -10,7 +10,6 @@ import axios from "axios";
 
 export default function Header({ updateCart, ...props }) {
   const navigate = useNavigate();
-  const dispatch = useDispatch()
   const authStatus = useSelector((state) => state.auth.status)
   const authData = useSelector((state) => state.auth.userData)
   const [login, setLogin] = useState(false)
@@ -33,6 +32,14 @@ export default function Header({ updateCart, ...props }) {
     } catch (error) {
       console.log(error.response)
     }
+  }
+
+  const toogleMenu = () => {
+    const menuItens = document.querySelector('#menu')
+    if (menuItens.className.includes("hidden"))
+      menuItens.className = menuItens.className.replace("hidden", "flex")
+    else if (menuItens.className.includes("flex"))
+      menuItens.className = menuItens.className.replace("flex", "hidden")
   }
 
   useEffect(() => {
@@ -122,11 +129,19 @@ export default function Header({ updateCart, ...props }) {
 
   return (
     <header {...props}>
-      <div className="flex flex-row md:flex-col justify-start w-full mx-auto gap-10 md:gap-10 md:px-5 max-w-7xl">
-        <button onClick={() => navigate("/")}>
+      <div className="flex flex-row justify-start w-full mx-auto gap-10 max-w-7xl h-[46px] relative">
+        <Button
+          className="md:block hidden !p-2  !h-fit !w-fit rounded-[5px] bg-transparent -mr-6 hover:bg-gray-200"
+          onClick={() => toogleMenu()}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" height="30" viewBox="0 0 50 50" fill="#0a033c">
+          <path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"></path>
+          </svg>
+        </Button>
+        <button onClick={() => navigate("/")} className="w-full max-w-[180px]">
           <Img src="/images/img_logo.svg" alt="logo_one" className="h-[30px] w-[175px]" />
         </button>
-        <ul className="flex flex-row md:flex-col justify-start items-center md:w-full gap-6 md:gap-5">
+        <ul className="flex flex-row justify-start items-center gap-5 md:hidden md:absolute md:w-fit md:rounded-[5px] md:top-14 md:left-0 md:py-2 md:px-3 md:bg-gray-200" id="menu">
           {
             navItems.map((item, i) => item.active ? (
               <li
@@ -148,7 +163,7 @@ export default function Header({ updateCart, ...props }) {
                   }
                 }}
               >
-                <Text as="button" className="!text-gray-900 !font-medium" onClick={() => {
+                <Text as="button" className="!text-gray-900 !font-medium hover:!font-semibold" onClick={() => {
                   navigate(item.slug, { state: item.state })
                   if (item.state) history.pushState(item.state, "")
                 }}>
@@ -176,10 +191,10 @@ export default function Header({ updateCart, ...props }) {
             ) : null)
           }
         </ul>
-        <ul className="flex flex-row justify-between md:w-full gap-5 ml-auto">
+        <ul className="flex flex-row justify-end md:w-full gap-5 ml-auto">
           {
             navAuthItems.map((item) => item.active ? (
-              <li key={item.name} className="flex flex-row justify-start items-center pl-[3px] gap-2.5 ml-auto" onClick={() => item.action(item)}>
+              <li key={item.name} className="flex flex-row justify-start items-center pl-[3px] gap-2" onClick={() => item.action(item)}>
                 <Text as="button" className="!text-gray-900 text-right !font-medium" >
                   {item.name}
                 </Text>
