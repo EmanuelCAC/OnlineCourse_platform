4c2ed74e04472d4fc16207c6a132baa8a20c73b8
EmanuelCAC <emanuelc279@gmail.com>
2024-06-07 19:49:27 -0300

    Responsive subiscribe component

--- client/src/components/Subscribe/index.jsx
+++ client/src/components/Subscribe/index.jsx
@@ -24,68 +24,68 @@ export default function Subscribe({ ...props }) {
 
   return (
     <div {...props}>
-      <div className="flex flex-row justify-center w-full p-[50px] md:p-5 bg-black-900_01 max-w-7xl rounded-[20px]">
-        <div className="flex flex-row md:flex-col justify-between w-full my-[11px] md:gap-10">
-          <div className="flex flex-col items-start justify-start w-[16%] md:w-full gap-[57px]">
+      <div className="flex flex-row justify-center w-full md:w-[80%] p-[50px] md:p-5 bg-black-900_01 max-w-7xl rounded-[20px]">
+        <div className="flex flex-row justify-between md:justify-center w-full my-[11px] gap-3">
+          <div className="flex flex-col items-start justify-start w-fit gap-[57px] md:hidden">
             <Img
               src="/images/img_ellipse_169.png"
               alt="circleimage"
-              className="h-[60px] w-[60px] md:h-auto rounded-[50%]"
+              className="h-[60px] aspect-square sm:hidden rounded-[50%]"
             />
             <Img
               src="/images/img_ellipse_170.png"
               alt="circleimage_one"
-              className="h-[60px] w-[60px] md:h-auto ml-[127px] md:ml-5 rounded-[50%]"
+              className="h-[60px] aspect-square ml-[150%] rounded-[50%]"
             />
             <Img
               src="/images/img_ellipse_171.png"
               alt="circleimage_two"
-              className="h-[60px] w-[60px] md:h-auto ml-[30px] md:ml-0 sm:ml-5 rounded-[50%]"
+              className="h-[60px] aspect-square ml-[30%] sm:ml-5 rounded-[50%]"
             />
           </div>
-          <div className="flex flex-row md:flex-col justify-between items-center w-[75%] md:w-full md:gap-10">
-            <div className="flex flex-col items-center justify-start w-[66%] md:w-full gap-10">
+          <div className="flex flex-col items-center justify-start">
+            <div className="flex flex-col items-center justify-start w-full gap-10">
               <div className="flex flex-col items-center justify-start w-full pb-[3px] gap-[26px]">
-                <Heading size="2xl" as="h1" className="!text-white-A700 !font-metropolis text-center leading-[55px]">
+                <Heading size="2xl" as="h1" className="!text-white-A700 !font-metropolis text-center leading-[55px] md:text-4xl">
                   Subscribe For Get Update
                   <br />
                   Every New Courses
                 </Heading>
-                <Text as="p" className="!text-white-A700_b2 text-center">
+                <Text as="p" className="!text-white-A700_b2 text-center md:text-sm">
                   20k+ students daily learn with Educatsy. Subscribe for new courses.
                 </Text>
               </div>
-              <form className="flex flex-row sm:flex-col justify-center w-full sm:gap-5" onSubmit={(e) => submitHandler(e)}>
+              <form className="flex flex-row sm:flex-col justify-center w-full sm:gap-3" onSubmit={(e) => submitHandler(e)}>
                 <Input
                   shape="round"
                   name="email"
                   placeholder="enter your email"
                   value={email}
                   onChange={(e) => setEmail(e)}
-                  className="w-[77%] md:w-full font-medium"
+                  className="w-[77%] sm:w-full md:h-[50px] font-medium sm:rounded-[10px]"
                 />
-                <Button size="3xl" type='submit' className="sm:px-5 rounded-tr-[10px] rounded-br-[10px] font-medium min-w-[138px]">
+                <Button size="3xl" type='submit' className="sm:px-5 rounded-tr-[10px] rounded-br-[10px] sm:rounded-[10px] font-medium min-w-[138px] md:h-[50px]">
                   Subscribe
                 </Button>
               </form>
             </div>
-            <div className="flex flex-col items-end justify-start w-[22%] md:w-full gap-[57px]">
-              <Img
-                src="/images/img_ellipse_166.png"
-                alt="circleimage"
-                className="h-[60px] w-[60px] md:h-auto mr-[30px] sm:mr-5 rounded-[50%]"
-              />
-              <Img
-                src="/images/img_ellipse_167.png"
-                alt="circleimage"
-                className="h-[60px] w-[60px] md:h-auto mr-[127px] md:mr-5 rounded-[50%]"
-              />
-              <Img
-                src="/images/img_ellipse_168.png"
-                alt="circleimage"
-                className="h-[60px] w-[60px] md:h-auto rounded-[50%]"
-              />
-            </div>
+          </div>
+          <div className="flex flex-col items-end justify-start gap-[57px]  md:hidden">
+            <Img
+              src="/images/img_ellipse_166.png"
+              alt="circleimage"
+              className="h-[60px] aspect-square mr-[90%] rounded-[50%]"
+            />
+            <Img
+              src="/images/img_ellipse_167.png"
+              alt="circleimage"
+              className="h-[60px] aspect-square mr-[190%] rounded-[50%]"
+            />
+            <Img
+              src="/images/img_ellipse_168.png"
+              alt="circleimage"
+              className="h-[60px] aspect-square rounded-[50%]"
+            />
           </div>
         </div>
       </div>
